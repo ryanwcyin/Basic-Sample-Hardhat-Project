44de03dbc693035193127f0e5d5b6874d5aba461
Ryan <ryanwcyin@gmail.com>
2021-11-27 23:52:31 +0800

    Try to count the state variable

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.0;  // should be same as hardhat.config.js
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Yo, I'm contractor.");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,19 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     // hre == hardhat runtime environment
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');  // compile the given contract
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contracted deployed to:", waveContract.address);
+    console.log("Contracted deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait()
+
+    waveCount = await waveContract.getTotalWaves();
 
 };
 
